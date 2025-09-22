5d11f48fe9e5606eca20d4164a396bc3e14f1009
rameshwar0611 <rameshwardangi0611@gmail.com>
2025-09-22 08:01:12 +0530

    added Product Schema  and Category Schema

---
+++ backend/src/model/Product.js
@@ -0,0 +1,54 @@
+const { default: mongoose } = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  mrpPrice: {
+    type: Number,
+    required: true,
+  },
+  sellingPrice: {
+    type: Number,
+    required: true,
+  },
+  discountPercent: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: [String],
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Seller",
+    required: true,
+  },
+  size: {
+    type: String,
+    required: true,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
