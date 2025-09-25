0c7a8711981218862599e9e15531d161b316fb31
rameshwar0611 <rameshwardangi0611@gmail.com>
2025-09-25 15:14:39 +0530

    Add customTheme

---
+++ frontend/ecommerce-store/src/Theme/customeTheme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const customeTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      // main: "#FB8C00",
+      main: "#00927c",
+    },
+    secondary: {
+      main: "#EAF0F1",
+    },
+  },
+});
