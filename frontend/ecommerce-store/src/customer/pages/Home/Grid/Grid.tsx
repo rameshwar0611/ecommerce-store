23d09c587ed62ca9ba4be500591d273486f3ef2b
rameshwar0611 <rameshwardangi0611@gmail.com>
2025-09-25 15:17:13 +0530

    Added Grid

---
+++ frontend/ecommerce-store/src/customer/pages/Home/Grid/Grid.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Grid = () => {
+  return (
+    <div className="grid gap-4 grid-rows-12 grid-cols-12 lg:h-[600px] px-5 lg:px-20">
+      <div className="col-span-3 row-span-12 text-white rounded-md">
+        <img
+          className="w-full h-full object-cover rounded-md"
+          src="https://img.perniaspopupshop.com/catalog/product/z/o/ZOOP102451_1.jpg?impolicy=detailimageprod"
+          alt=""
+        />
+      </div>
+      <div className="col-span-2 row-span-6 text-white rounded-md">
+        <img
+          className="w-full h-full object-cover rounded-md"
+          src="https://img.perniaspopupshop.com/catalog/product/z/o/ZOOP102451_1.jpg?impolicy=detailimageprod"
+          alt=""
+        />
+      </div>
+      <div className="col-span-4 row-span-6 text-white rounded-md">
+        <img
+          className="w-full h-full object-cover rounded-md"
+          src="https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/23807268/2023/6/29/9930b235-5318-4755-abbe-08f99e969e781688026636544LehengaCholi7.jpg"
+          alt=""
+        />
+      </div>
+      <div className="col-span-3 row-span-12  text-white rounded-md">
+        <img
+          className="w-full h-full object-cover rounded-md"
+          src="https://
+images.pexels.com/photos/12730873/pexels-photo-12730873.jpeg?
+auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+      </div>
+      <div className="col-span-4 row-span-6 text-white rounded-md">
+        <img
+          className="w-full h-full object-cover rounded-md"
+          src="https://img.perniaspopupshop.com/catalog/product/z/o/ZOOP102451_1.jpg?impolicy=detailimageprod"
+          alt=""
+        />
+      </div>
+      <div className="col-span-2 row-span-6 text-white rounded-md">
+        <img
+          className="w-full h-full object-cover rounded-md"
+          src="https://img.perniaspopupshop.com/catalog/product/z/o/ZOOP102451_1.jpg?impolicy=detailimageprod"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Grid;
